c508a1512a68a2c4fe9376a8250d80a09bdc0beb
dempseyt <dempsey.thompson@journeyone.com.au>
2024-02-21 17:04:33 +0800

    GREEN: Given no initial value and an empty array throws an error

--- src/reduce.js
+++ src/reduce.js
@@ -4,6 +4,11 @@ function reduce(array, reducer, initialValue) {
   let accumulator;
   let startPoint;
   if (initialValue === undefined) {
+    if (array.length === 0) {
+      throw new Error(
+        "An initial value must be provided when reducing over an empty array",
+      );
+    }
     accumulator = array[0];
     startPoint = 1;
   } else {
