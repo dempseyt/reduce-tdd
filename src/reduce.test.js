cae44ebd23e1821db46c33fa22c8fca9b754e70e
dempseyt <dempsey.thompson@journeyone.com.au>
2024-02-21 15:50:26 +0800

    RED/GREEN: does not modify the original array

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -19,4 +19,13 @@ describe("reduce", () => {
     const sumOfNumbersInAnArray = reduce(array, reducer, initial);
     expect(sumOfNumbersInAnArray).toBe(14);
   });
+  it("does not modify the original array", () => {
+    const array = [1, 2, 3, 4];
+    const initialValue = 4;
+    function reducer(accumulator, currentItem) {
+      return accumulator + currentItem;
+    }
+    reduce(array, reducer, initialValue);
+    expect(array).toStrictEqual([1, 2, 3, 4]);
+  });
 });
