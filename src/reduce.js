5636501f2be45aa8f24ef01fffdd20926de198a0
dempseyt <dempsey.thompson@journeyone.com.au>
2024-02-21 15:42:36 +0800

    GREEN: should use initial value as accumulator instead of first element

--- src/reduce.js
+++ src/reduce.js
@@ -1,6 +1,16 @@
-function reduce(array, reducer) {
-  let accumulator = array[0];
-  for (let i = 1; i < array.length; i++) {
+import { start } from "repl";
+
+function reduce(array, reducer, initialValue) {
+  let accumulator;
+  let startPoint;
+  if (initialValue === undefined) {
+    accumulator = array[0];
+    startPoint = 1;
+  } else {
+    accumulator = initialValue;
+    startPoint = 0;
+  }
+  for (let i = startPoint; i < array.length; i++) {
     accumulator = reducer(accumulator, array[i]);
   }
   return accumulator;
